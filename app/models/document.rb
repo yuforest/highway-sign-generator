8eae8333fd66a77a372f664858ebe917fea43ca0
大森優太 <>
2020-08-19 15:54:24 +0900

    バリデーションと注意書きを追加

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,8 @@
 class Document < ApplicationRecord
-  validates :content, presence: true
+  validates :content, presence: true,
+                      format: {
+                        with: /\A[ぁ-んァ-ヶ0-9０-９ｧ-ﾝﾞﾟー－]+\z/,
+                        message: "全角のひらがな、全角か半角のカタカナ、数字のみ利用可能です。"
+                      }
   is_impressionable
 end
